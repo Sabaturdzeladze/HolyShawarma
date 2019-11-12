59783353d1255e05a2f6b349644fc23008c54624
sabaturdzeladze <sabaturdzeladze@gmail.com>
2019-11-12 18:23:07 +0400

    logout logic added

--- App/navigation/HomeNavigation.js
+++ App/navigation/HomeNavigation.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import HomeScreen from '../screens/HomeScreen';
 
@@ -8,9 +6,6 @@ const HomeNavigator = createStackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-      headerRight: (
-        <Icon name="logout" size={23} color="#fff" onPress={() => {}} />
-      ),
       headerRightContainerStyle: {
         backgroundColor: '#FF5908',
         width: '15%',
--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import {ScrollView, Text, StyleSheet} from 'react-native';
+import React, { useCallback, useEffect } from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useDispatch } from 'react-redux';
 
 import Card from '../components/UI/Card';
 import CopyableText from '../components/UI/CopyableText';
 import CustomizeOrder from '../components/CustomizeOrder';
-
-
+import * as userActions from '../../store/actions/user';
 
 const HomeScreen = props => {
+  const dispatch = useDispatch();
+
+  const { navigation } = props;
+  
+  const logout = useCallback(async () => {
+    await dispatch(userActions.logout());
+    navigation.navigate('Auth');
+  }, [dispatch, navigation]);
+
+  useEffect(() => {
+    navigation.setParams({ logout });
+  }, [dispatch]);
+  
   return (
     <ScrollView style={styles.screen}>
       <Card style={styles.accounts}>
-        <CopyableText text="TBC: " account="GE04TB7425645061600033"/>
+        <CopyableText text="TBC: " account="GE04TB7425645061600033" />
         <CopyableText text="BG: " account="GE04TB7425645061600033" />
       </Card>
       <CustomizeOrder />
@@ -20,32 +34,35 @@ const HomeScreen = props => {
 };
 
 HomeScreen.navigationOptions = navData => {
+  const logout = navData.navigation.getParam('logout');
+  
   return {
- 
-
     headerTitle: 'დაამატე შეკვეთა',
     headerTitleContainerStyle: {
-      backgroundColor:'#FF5908',
+      backgroundColor: '#FF5908',
       alignItems: 'center',
-      justifyContent:'center'
+      justifyContent: 'center',
     },
     headerTitleStyle: {
       fontSize: 15,
-      color:'#fff' ,
+      color: '#fff',
       fontWeight: 'bold',
-    
-    }
-  }
-}
+    },
+    headerRight: (
+      <Icon
+        name="logout"
+        color="#fff"
+        size={23}
+        onPress={logout}
+      />
+    ),
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1
-  },
-  accounts: {
-    padding: 10,
-    margin: 10
+    flex: 1,
   }
-})
+});
 
 export default HomeScreen;

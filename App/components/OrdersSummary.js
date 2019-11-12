d22a42c106a8bf55564f2d8f93cb03e84443f0c1
Anakin0101 <mdivani1@mail.com>
2019-11-12 16:20:29 +0400

    styled app

--- App/components/OrdersSummary.js
+++ App/components/OrdersSummary.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Card from '../components/UI/Card';
 
 import CopyableText from './UI/CopyableText';
 
 const OrdersSummary = ({orders}) => {
   const {length} = orders;
-  
+
   return (
-    <View>
-      <Text>რაოდენობა: {length}</Text>
-      <Text>თანხა: {length * 9}</Text>
-      <Text>კომენტარი საშაურმეს (გონებაშეზღუდულთათვის):</Text>
-      <CopyableText />
+    <View style={{alignItems: 'center'}}>
+      <Card style={styles.comment}>
+        <Text>
+          რაოდენობა:{' '}
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{length}</Text>
+        </Text>
+        <Text>
+          თანხა:{' '}
+          <Text style={{fontWeight: 'bold', fontSize: 15}}>{length * 9}</Text>
+        </Text>
+        <Text style={{textAlign: 'center'}}>
+          კომენტარი საშაურმეს (გონებაშეზღუდულთათვის):
+        </Text>
+        <CopyableText />
+      </Card>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  comment: {
+    alignItems: 'center',
+  },
+});
 
 export default OrdersSummary;
--- store/reducers/user.js
+++ store/reducers/user.js
@@ -1,13 +1,21 @@
 import {USER_LOGIN} from '../actions/user';
+import {USER_LOGOUT} from '../actions/user';
+import AsyncStorage from '@react-native-community/async-storage';
 
 let initialState = {
   user: {},
 };
 
-export default (state = initialState, action) => {
+export default async (state = initialState, action) => {
   switch (action.type) {
     case USER_LOGIN:
+      await AsyncStorage.setItem('username', userName);
+      await AsyncStorage.setItem('password', password);
       return {...state, user: action.user};
+    case USER_LOGOUT:
+      await AsyncStorage.removeItem('username');
+      await AsyncStorage.removeItem('password');
+      return {...initialState};
   }
 
   return state;

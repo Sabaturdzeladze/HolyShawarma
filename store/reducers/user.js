34b40302ae0471bd97324dcf0158479e2402b6ff
Anakin0101 <mdivani1@mail.com>
2019-11-12 16:27:30 +0400

    fixed bug on user login

--- store/reducers/user.js
+++ store/reducers/user.js
@@ -9,8 +9,8 @@ let initialState = {
 export default async (state = initialState, action) => {
   switch (action.type) {
     case USER_LOGIN:
-      await AsyncStorage.setItem('username', userName);
-      await AsyncStorage.setItem('password', password);
+      await AsyncStorage.setItem('username', action.user.userName);
+      await AsyncStorage.setItem('password', action.user.password);
       return {...state, user: action.user};
     case USER_LOGOUT:
       await AsyncStorage.removeItem('username');
